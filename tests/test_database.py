cdbc625f79629aabd8497e1b0fe4fb8fe9090ef5
Anton Glazunov <tagl91@yandex.ru>
2024-06-04 19:04:02 +0300

    add test utils

--- tests/test_database.py
+++ tests/test_database.py
@@ -14,5 +14,7 @@ def test_create_db(con_db):
 
 
 def test_create_table(con_db):
-    assert con_db.create_table("test_table", ["id_test int PRIMARY KEY", "text_test text"]) == "Таблица test_table с столбцами id_test int PRIMARY KEY, text_test text успешно создана"
-    assert con_db.add_info("test_table", ["(1)", "(2)", "(3)"]) == "Информация (1), (2), (3) в таблицу test_table добавлена"
+    assert con_db.create_table("test_table", ["id_test int PRIMARY KEY",
+                                              "text_test text"]) == "Таблица test_table с столбцами id_test int PRIMARY KEY, text_test text успешно создана"
+    assert con_db.add_info("test_table",
+                           ["(1)", "(2)", "(3)"]) == "Информация (1), (2), (3) в таблицу test_table добавлена"
